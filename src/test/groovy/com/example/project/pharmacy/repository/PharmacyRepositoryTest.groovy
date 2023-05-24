67525cc4d42d0e0c014c34453a04edda2b0ce7e0
yujin-17 <lyjyellow@gmail.com>
2023-05-24 23:17:56 +0900

    Test: 통합 테스트코드 작성

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,11 +4,14 @@ import com.example.project.AbstractIntegrationContainerBaseTest
 import com.example.project.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
-class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest { // 테스트 컨테이너를 이용한 통합 테스트환경 구성 -> 이 테스트코드를  실행 하기 전에 도커가 실행되어있어야함.
 
     @Autowired  // 빈 주입
     private PharmacyRepository pharmacyRepository
 
+    def setup(){ // test 시작 전에 실행되는 메서드
+        pharmacyRepository.deleteAll() // 테스트 시작 전에 데이터 정리
+    }
     def "PharmacyRepository save"() {
         given:
         String address = "서울 특별시 성북구 종암동"
@@ -34,5 +37,26 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     }
 
+    def "PharmacyRepository saveAll"(){
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+    }
 
 }
