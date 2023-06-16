db3ea7b3f1447c1c01d77272236aa1e036894418
yujin-17 <lyjyellow@gmail.com>
2023-06-16 12:02:56 +0900

    Feat: 거리 계산 알고리즘 테스트코드 작성

--- src/main/java/com/example/project/direction/service/DirectionService.java
+++ src/main/java/com/example/project/direction/service/DirectionService.java
@@ -4,7 +4,12 @@ import com.example.project.api.dto.DocumentDto;
 import com.example.project.direction.entity.Direction;
 import com.example.project.pharmacy.dto.PharmacyDto;
 import com.example.project.pharmacy.service.PharmacySearchService;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,16 +19,40 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DirectionService {
 
+  private static final int MAX_SEARCH_COUNT = 3; // 약국 최대 검색 갯수
+  private static final double RADIUS_KM = 10.0; // 반경 10 km
   private final PharmacySearchService pharmacySearchService;
 
   public List<Direction> buildDirectionList(DocumentDto documentDto) {
 
-    // 약국 데이터 조회
-    List<PharmacyDto> pharmacyDtos = pharmacySearchService.searchPharmacyDtoList();
+    if(Objects.isNull(documentDto)) return Collections.emptyList();
+
+    return pharmacySearchService.searchPharmacyDtoList()
+        .stream().map(pharmacyDto ->
+            Direction.builder()
+                .inputAddress(documentDto.getAddressName())
+                .inputLatitude(documentDto.getLatitude())
+                .inputLongitude(documentDto.getLongitude())
+                .targetPharmacyName(pharmacyDto.getPharmacyName())
+                .targetAddress(pharmacyDto.getPharmacyAddress())
+                .targetLatitude(pharmacyDto.getLatitude())
+                .targetLongitude(pharmacyDto.getLongitude())
+                .distance(
+                    calculateDistance(documentDto.getLatitude(), documentDto.getLongitude(),
+                        pharmacyDto.getLatitude(), pharmacyDto.getLongitude())
+                )
+                .build())
+        .filter(direction -> direction.getDistance() <= RADIUS_KM) // 반경 10km 이내에 있는 것만
+        .sorted(Comparator.comparing(Direction::getDistance)) // 가까운 약국 순서대로 sort
+        .limit(MAX_SEARCH_COUNT) // 최대 3개
+        .collect(Collectors.toList());
+
+    }
+
 
     // 거리계산 알고리즘을 이용하여, 고객과 약국 사이의 거리를 계산하고 sort
 
-  }
+
 
   // Haversine formula -> 고객과 약국 사이의 거리를 구하는 공식
   private double calculateDistance(double lat1, double lon1, double lat2, double lon2) { // 첫번째 위도 경도 = 고객의 위치 데이터, 두번째 위도 경도 = 약국의 위치 데이터
