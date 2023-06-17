25553ec0f3936ae415ee45763ec65c8bab8ddc45
yujin-17 <lyjyellow@gmail.com>
2023-06-17 19:42:16 +0900

    Feat: Spring retry 구현  및 검증

--- src/main/java/com/example/project/api/service/KakaoAddressSearchService.java
+++ src/main/java/com/example/project/api/service/KakaoAddressSearchService.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
@@ -24,11 +27,16 @@ public class KakaoAddressSearchService {
   @Value("${kakao.rest.api.key}") // 환경변수 가져오기
   private String kakaoRestApiKey;
 
+  @Retryable(
+      value = {RuntimeException.class}, // 상황에 따라 더 구체적인 전처리도 가능하다
+      maxAttempts = 2, // 2번의 재시도를 허용 (기본값 3번)
+      backoff = @Backoff(delay = 2000) // 얼마만큼 딜레이를 줄건지 = 2초
+  )
   public KakaoApiResponseDto requestAddressSearch(String address){
 
     if(ObjectUtils.isEmpty(address)) return null;
 
-    URI uri = kakaoUriBuilderService.builderUriByAddressSearch(address);
+    URI uri = kakaoUriBuilderService.builderUriByAddressSearch(address); // 실제 카카오 API를 호출하기 위한 api
 
     HttpHeaders headers = new HttpHeaders();
     headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
@@ -37,5 +45,10 @@ public class KakaoAddressSearchService {
     // kakao api 호출
     return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
   }
+  @Recover
+  public KakaoApiResponseDto recover(RuntimeException e, String address) {
+    log.error("All the retries failed. address: {}, error: {}", address, e.getMessage());
+    return null;
+  }// @Recover를 사용할때의 주의점은, 원래 메소드의 리턴 타입을 맞춰줘야한다.
 
 }
---
+++ src/test/groovy/com/example/project/api/service/KakaoAddressSearchServiceRetryTest.groovy
@@ -0,0 +1,80 @@
+package com.example.project.api.service
+
+import com.example.project.AbstractIntegrationContainerBaseTest
+import com.example.project.api.dto.DocumentDto
+import com.example.project.api.dto.KakaoApiResponseDto
+import com.example.project.api.dto.MetaDto
+import com.fasterxml.jackson.databind.ObjectMapper
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+
+class KakaoAddressSearchServiceRetryTest extends AbstractIntegrationContainerBaseTest { // 통합 테스트 환경에서 진행
+
+    @Autowired
+    private KakaoAddressSearchService kakaoAddressSearchService
+
+    @SpringBean // spock에서는 SpringBean을 사용하고, mockito를 사용하는 경우 MockBean을 사용. MockBean은 Spring 컨테이너 내에 있는 빈을 목킹한다는 의미. // 실제 카카오 uri api를 가져오는게 아니기 때문에 목킹함.
+    private KakaoUriBuilderService kakaoUriBuilderService = Mock()
+
+    private MockWebServer mockWebServer
+
+    private ObjectMapper mapper = new ObjectMapper()
+
+    private String inputAddress = "서울 성북구 종암로 10길"
+
+    def setup() {
+        mockWebServer = new MockWebServer()
+        mockWebServer.start()
+    }
+
+    def cleanup() { // test 메서드가 끝나고 난 후 실행되는 메서드
+        mockWebServer.shutdown()
+    }
+
+    def "requestAddressSearch retry success"() {
+        given:
+        def metaDto = new MetaDto(1)
+        def documentDto = DocumentDto.builder()
+                .addressName(inputAddress)
+                .build()
+        def expectedResponse = new KakaoApiResponseDto(metaDto, Arrays.asList(documentDto))
+        def uri = mockWebServer.url("/").uri()
+
+        when:
+        mockWebServer.enqueue(new MockResponse().setResponseCode(504))
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200)
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(mapper.writeValueAsString(expectedResponse)))
+
+        def kakaoApiResult = kakaoAddressSearchService.requestAddressSearch(inputAddress)
+        def takeRequest = mockWebServer.takeRequest()
+
+        then:
+        2 * kakaoUriBuilderService.builderUriByAddressSearch(inputAddress) >> uri
+        takeRequest.getMethod() == "GET"
+        kakaoApiResult.getDocumentList().size() == 1
+        kakaoApiResult.getMetaDto().totalCount == 1
+        kakaoApiResult.getDocumentList().get(0).getAddressName() == inputAddress
+
+    }
+
+
+    def "requestAddressSearch retry fail "() {
+        given:
+        def uri = mockWebServer.url("/").uri()
+
+        when:
+        mockWebServer.enqueue(new MockResponse().setResponseCode(504))
+        mockWebServer.enqueue(new MockResponse().setResponseCode(504))
+
+        def result = kakaoAddressSearchService.requestAddressSearch(inputAddress)
+
+        then:
+        2 * kakaoUriBuilderService.builderUriByAddressSearch(inputAddress) >> uri
+        result == null
+    }
+}
